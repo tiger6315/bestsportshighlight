119f0c97118c3dbd34aa9c370d14406a0d171580
Tiger6315 <152846730+tiger6315@users.noreply.github.com>
2025-02-21 11:18:24 +0200

    correct widget

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1 class="title">BESTSPORTSHIGHLIGHT</h1>
     <div class="grid">
-      <div v-for="(match, index) in matches" :key="index" class="widget-container sr-widget-1">
+      <div v-for="(match, index) in matches" :key="index" :id="'sr-widget-' + index" class="widget-container">
       </div>
     </div>
   </div>
@@ -11,18 +11,21 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 
-const matches = ref(["match_1", "match_2", "match_3", "match_4"]);
+const matches = ref(["52631971", "52631972", "52631973", "52631974"]);
 
 onMounted(() => {
-  (function(a,b,c,d,e,f,g,h,i){
-    a[e]||(i=a[e]=function(){(a[e].q=a[e].q||[]).push(arguments)},i.l=1*new Date,i.o=f,
-    g=b.createElement(c),h=b.getElementsByTagName(c)[0],g.async=1,g.src=d,g.setAttribute("n",e),h.parentNode.insertBefore(g,h)
-  })(window,document,"script", "https://widgets.sir.sportradar.com/lmtdemo/widgetloader", "SIR", {
-      theme: false, // using custom theme
-      language: "en"
-  });
-  
-  SIR("addWidget", ".sr-widget-1", "match.preview", {matchId: 52631971});
+  const script = document.createElement("script");
+  script.src = "https://widgets.sir.sportradar.com/sportradar/widgetloader";
+  script.async = true;
+  document.body.appendChild(script);
+
+  script.onload = () => {
+    if (window.SIR) {
+      matches.value.forEach((match, index) => {
+        window.SIR('addWidget', `#sr-widget-${index}`, 'match.lmtPlus', { matchId: match });
+      });
+    }
+  };
 });
 </script>
 
